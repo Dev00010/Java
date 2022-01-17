f5fcb5b947e56a6a1885a51fb86c5fae0f864bde
Dev <joisardev@gmail.com>
2022-01-17 22:14:48 +0530

    Swap 2 values without using 3rd value

---
+++ SwapWithout3rd.java
@@ -0,0 +1,16 @@
+public class SwapWithout3rd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b;
+		a=10;
+		b=20;
+		System.out.println("A=> "+a +"B=>"+b);
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("A=> "+a +"B=>"+b);
+	}
+
+}
