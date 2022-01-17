55af3295b54d9d6a8d80cd0253d8f872f2a584b1
Dev <joisardev@gmail.com>
2022-01-17 22:16:38 +0530

    Print transpose of a matrices

---
+++ TransposeMatrices.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Transpose {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[][]=new int[100][100];
+		int b[][]=new int[100][100];
+		int n,i,j;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Number=>");
+		n=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				System.out.print("Enter Number for a["+i+"]["+j+"]=> ");
+				a[i][j]=sc.nextInt();
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				b[i][j]=a[j][i];
+				System.out.print(b[i][j]);
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
