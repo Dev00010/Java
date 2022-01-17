b315fa12294c0851c22b130829438e65567b0fd5
Dev <joisardev@gmail.com>
2022-01-17 22:00:56 +0530

    For Loop Demo

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		
+		int i;
+		for(i=1;i<=20;i++)
+		{
+			System.out.println(i + "Hi");
+		}
+
+	}
+
+}
