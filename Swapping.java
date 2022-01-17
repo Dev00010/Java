ab2992dc028b95353c2ab33e05744553f6972655
Dev <joisardev@gmail.com>
2022-01-17 22:14:29 +0530

    Swap 2 values

---
+++ Swapping.java
@@ -0,0 +1,17 @@
+public class Swapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,c;
+		a=5;
+		b=10;
+		System.out.println("A=> "+a + " B=> "+b);
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("A=> "+a + " B=> "+b);
+
+	}
+
+}
