f8c6522bb083f74c52b498b42664b18cc525a3bb
Dev <joisardev@gmail.com>
2022-01-17 16:51:53 +0530

    Prints area of circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,15 @@
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int r;
+		double area;
+		r=10;
+		area=3.14*r*r;
+		System.out.println("Area=> "+ area);
+
+	}
+
+}
