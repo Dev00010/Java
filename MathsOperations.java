85cba25537da71a1bf6c842860c29f1828474485
Dev <joisardev@gmail.com>
2022-01-17 22:06:25 +0530

    Maths Operations

---
+++ MathsOperations.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a,b,add,sub,mult;
+		double div;
+		a=10;
+		b=20;
+		add=a+b;
+		sub=a-b;
+		div=a/b;
+		mult=a*b;
+		System.out.println("Add=> "+ add);
+		System.out.println("Sub=> "+ sub);
+		System.out.println("Div=> "+ div);
+		System.out.println("Mult=> "+ mult);
+
+	}
+
+}
